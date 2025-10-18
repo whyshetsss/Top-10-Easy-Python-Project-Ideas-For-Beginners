aeb23ca066008bfe86eff36dcd0992234d47cf9c
whyshetsss <ilya2000_fifa@mail.ru>
2025-10-18 17:59:05 +0300

    Refactor calculator app to use main guard

--- calculator.py
+++ calculator.py
@@ -1,27 +1,30 @@
 def add(a, b):
     """Функция сложения для тестов"""
     return a + b
-print("===== Welcome to Calculator App =====\n")
-while 1:
-    print("What would you like to do:-")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3  Multiplication")
-    print("4. Division")
-    print("5. Exit")
-    choice = int(input("Enter your choice: "))
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    if choice == 1:    
-        print("Output: ", num1+num2)
-    elif choice == 2:
-        print("Output: ", num1-num2)
-    elif choice == 3:
-        print("Output: ", num1*num2)
-    elif choice == 4:
-        print("Output: ", num1/num2)
-    elif choice == 5:
-        print("Exiting...")
-        break
-    print()
 
+
+if __name__ == "__main__":
+    print("===== Welcome to Calculator App =====\n")
+    while True:
+        print("What would you like to do:-")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3  Multiplication")
+        print("4. Division")
+        print("5. Exit")
+        choice = int(input("Enter your choice: "))
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+        if choice == 1:
+            print("Output:", num1 + num2)
+        elif choice == 2:
+            print("Output:", num1 - num2)
+        elif choice == 3:
+            print("Output:", num1 * num2)
+        elif choice == 4:
+            print("Output:", num1 / num2)
+        elif choice == 5:
+            print("Exiting...")
+            break
+        print()
