c1062bf0478bd379cf8902995116d6c6d610d5ca
whyshetsss <ilya2000_fifa@mail.ru>
2025-10-18 17:42:17 +0300

    unit test for calculator
    Add unit tests for the addition function in calculator.

---
+++ tests/test_calculator.py
@@ -0,0 +1,8 @@
+import pytest
+from calculator import add
+
+def test_addition():
+    """Проверяем базовую работу функции сложения"""
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
