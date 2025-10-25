98f92e2342c20f13674b008d5b294730a84fbec8
whyshetsss <ilya2000_fifa@mail.ru>
2025-10-25 22:30:02 +0300

    Add multiply function to test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
-from calculator import add, subtract
+from calculator import add, subtract, multiply
     
 def test_add():
     assert add(2, 3) == 5
