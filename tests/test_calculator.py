3668550cf66e99102507ae6d59cc4c5e0488847a
whyshetsss <ilya2000_fifa@mail.ru>
2025-10-25 22:19:43 +0300

    Import subtract function in test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
-from calculator import add
+from calculator import add, subtract
     
 def test_add():
     assert add(2, 3) == 5
